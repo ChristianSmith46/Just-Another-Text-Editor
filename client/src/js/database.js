f099264bf2bfd2c3c74f7adb35cf8f1502fb8396
Christian Smith <christian.46.smith@gmail.com>
2022-12-01 11:51:08 -0500

    Added a gitignore and implemented getDb and postDb in the database.js to store and retrieve data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+
+  const transaction = jateDb.transaction('jate', 'readwrite');
+
+  const store = transaction.objectStore('jate');
+
+  const request = store.add(content);
+
+  const result = await request;
+  console.log(`putDb: ${result}`);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+
+  const transaction = jateDb.transaction('jate', 'readonly');
+
+  const store = transaction.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log(`getDb: ${result}`);
+  return result;
+}
 
 initdb();
