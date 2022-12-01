efb80dfcdd65ef45b8d2046423268dc6d29f80ed
Christian Smith <christian.46.smith@gmail.com>
2022-12-01 13:54:00 -0500

    Add comments to the src-sw.js for the asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,11 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+// Implement asset caching from syles scripts and workers
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
+    // Set the name to asset-cache
     cacheName: 'asset-cache',
     plugins: [
+      // Use cacheableResponsePlugin to cache the responses when status is 0-200
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
